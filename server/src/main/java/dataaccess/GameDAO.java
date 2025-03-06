9cedfcf5532f907116305a28aacfb8fbd0d9cffc
Josh-Weidner <josh.weidner2@gmail.com>
2025-03-05 22:36:19 -0700

    update game dao signatures

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -2,14 +2,15 @@ package dataaccess;
 
 import chess.ChessGame;
 import model.GameData;
+import server.ResponseException;
 
 import java.util.ArrayList;
 
 public interface GameDAO {
-    Integer createGame(String gameName);
-    void updateGame(Integer gameId, ChessGame.TeamColor teamColor, String userName);
-    GameData getGame(Integer gameId);
-    ArrayList<GameData> listGames();
-    void clear();
+    Integer createGame(String gameName) throws ResponseException;
+    void updateGame(Integer gameId, ChessGame.TeamColor teamColor, String userName) throws ResponseException;
+    GameData getGame(Integer gameId) throws ResponseException;
+    ArrayList<GameData> listGames() throws ResponseException;
+    void clear() throws ResponseException;
 
 }
