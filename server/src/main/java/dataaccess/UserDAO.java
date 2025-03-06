5a43a48d471e49f8f374c1d845232a59ebe242de
Josh-Weidner <josh.weidner2@gmail.com>
2025-03-05 22:36:04 -0700

    update user dao signatures

--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -1,10 +1,11 @@
 package dataaccess;
 
 import model.UserData;
+import server.ResponseException;
 
 public interface UserDAO {
-    UserData getUser(String username);
+    UserData getUser(String username) throws ResponseException;
 
-    void addUser(UserData user);
-    void clear();
+    void addUser(UserData user) throws ResponseException;
+    void clear() throws ResponseException;
 }
