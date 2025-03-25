912135b94f200dad60315f0539f7a30a1047c029
Josh-Weidner <josh.weidner2@gmail.com>
2025-03-24 20:27:59 -0600

    implementing starting ui

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,26 @@
-import chess.*;
+// import chess.*;
+
+import static ui.EscapeSequences.*;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+//        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+//        System.out.println("♕ 240 Chess Client: " + piece);
+
+        System.out.println();
+        System.out.println(BLACK_QUEEN + "Welcome to 240 Chess. Type 'ready' to get started." + BLACK_QUEEN);
+        // If not logged in
+            try {
+                String command = "export PS1='[LOGGED_OUT] >>> '";
+                ProcessBuilder pb = new ProcessBuilder("bash", "-c", command);
+                pb.inheritIO().start().waitFor();
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        System.out.println(SET_TEXT_COLOR_MAGENTA + "    register <USERNAME> <PASSWORD> <EMAIL>" + RESET_TEXT_COLOR + " - to create an account");
+        System.out.println(SET_TEXT_COLOR_MAGENTA + "    login <USERNAME> <PASSWORD>" + RESET_TEXT_COLOR + " - to play chess");
+        System.out.println(SET_TEXT_COLOR_MAGENTA + "    quit" + RESET_TEXT_COLOR + " - exit program");
+        System.out.println(SET_TEXT_COLOR_MAGENTA + "    help" + RESET_TEXT_COLOR + " - possible commands");
     }
 }
