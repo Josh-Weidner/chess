ce41034e3d133ea44d2771be1084cfd3efa2a65c
Josh-Weidner <josh.weidner2@gmail.com>
2025-03-24 22:38:13 -0600

    handle register and login commands

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,5 +1,3 @@
-import chess.*;
-
 import com.google.gson.Gson;
 import server.Server;
 import service.login.LoginRequest;
@@ -8,90 +6,119 @@ import service.register.RegisterRequest;
 import service.register.RegisterResult;
 
 import static ui.EscapeSequences.*;
-import java.io.IOException;
+
 import java.net.MalformedURLException;
 import java.util.Scanner;
 
 public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
+    static Gson gson = new Gson();
+    Server server = new Server();
+    static int port = server.run(800);
+    static ServerFacade serverFacade = new ServerFacade(Integer.toString(port));
+    static String registeredUsername = "";
+
     public static void main(String[] args) throws MalformedURLException {
-        Scanner scanner = new Scanner(System.in);
-        Gson gson = new Gson();
-        Server server = new Server();
-        int port = server.run(800);
-        ServerFacade serverFacade = new ServerFacade(Integer.toString(port));
-        String registeredUsername = "";
-        String prompt = !registeredUsername.equals("") ? "[" + registeredUsername.toUpperCase() + "] >>> " : "[LOGGED_OUT] >>> ";
 //        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
 //        System.out.println("♕ 240 Chess Client: " + piece);
 
         System.out.println();
-        System.out.println(BLACK_QUEEN + "Chess 240. Type 'ready' to get started." + BLACK_QUEEN);
+        System.out.println(BLACK_QUEEN + "Chess 240. Type 'help' to get started." + BLACK_QUEEN);
         System.out.println();
 
-        System.out.print(prompt);
-        String input = scanner.nextLine();
+        NextCommand(registeredUsername);
+    }
 
-        if (input.equalsIgnoreCase("ready")) {
-            displayCommands(registeredUsername);
-        }
-
+    private static void NextCommand(String username) {
+        String prompt = !username.isEmpty() ? "[" + username.toUpperCase() + "] >>> " : "[LOGGED_OUT] >>> ";
         System.out.print(prompt);
         String cmd = scanner.next();
 
-        if (cmd.equalsIgnoreCase("register")) {
-            try {
-                String username = scanner.next();
-                String password = scanner.next();
-                String email = scanner.next();
-
-                String registerRequest = new RegisterRequest(username, password, email).toString();
-                String json = gson.toJson(registerRequest);
-
-                String result = serverFacade.registerUser(json);
-
-                RegisterResult registerResult = gson.fromJson(result, RegisterResult.class);
-
-                registeredUsername = registerResult.username();
-
-                System.out.println("Welcome " + SET_TEXT_BOLD + registeredUsername + "!");
+        if (prompt.equals("[LOGGED_OUT] >>> ")) {
+            if (cmd.equalsIgnoreCase("register")) {
+                Register();
             }
-            catch (Exception e) {
-                System.out.println(e.getMessage());
-                // TODO: if not enough arguments ask again
-                // TODO: if cant register user, prompt for new username
+            else if (cmd.equalsIgnoreCase("login")) {
+                Login();
+            }
+            else if (cmd.equalsIgnoreCase("quit")) {
+                Quit();
+            }
+            else if (cmd.equalsIgnoreCase("help")) {
+                DisplayCommands(username);
             }
         }
-
-        else if (cmd.equalsIgnoreCase("login")) {
-            try {
-                String username = scanner.next();
-                String password = scanner.next();
-
-                String loginRequest = new LoginRequest(username, password).toString();
-                String json = gson.toJson(loginRequest);
-
-                String result = serverFacade.loginUser(json);
-                LoginResult loginResult = gson.fromJson(result, LoginResult.class);
-
-                registeredUsername = loginResult.username();
-                System.out.println("Welcome " + SET_TEXT_BOLD + registeredUsername + "!");
+        else {
+            if (cmd.equalsIgnoreCase("create")) {
+                Create();
             }
-            catch (Exception e) {
-                System.out.println(e.getMessage());
-                // TODO: request valid credentials
+            else if (cmd.equalsIgnoreCase("list")) {
+                List();
             }
-        }
-
-        else if (cmd.equalsIgnoreCase("quit")) {
-            System.exit(0);
-        }
-
-        else if (cmd.equalsIgnoreCase("help")) {
-            displayCommands(registeredUsername);
+            else if (cmd.equalsIgnoreCase("join")) {
+                Join();
+            }
+            else if (cmd.equalsIgnoreCase("observe")) {
+                Observe();
+            }
+            else if (cmd.equalsIgnoreCase("logout")) {
+                Logout();
+            }
+            else if (cmd.equalsIgnoreCase("quit")) {
+                Quit();
+            }
+            else if (cmd.equalsIgnoreCase("help")) {
+                DisplayCommands(username);
+            }
         }
     }
 
-    private static void displayCommands(String username) {
+    private static void Register() {
+        try {
+            String username = scanner.next();
+            String password = scanner.next();
+            String email = scanner.next();
+
+            String registerRequest = new RegisterRequest(username, password, email).toString();
+            String json = gson.toJson(registerRequest);
+
+            String result = serverFacade.registerUser(json);
+
+            RegisterResult registerResult = gson.fromJson(result, RegisterResult.class);
+
+            registeredUsername = registerResult.username();
+
+            System.out.println("Welcome " + SET_TEXT_BOLD + registeredUsername + "!");
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            // TODO: if not enough arguments ask again
+            // TODO: if cant register user, prompt for new username
+        }
+    }
+
+    private static void Login() {
+        try {
+            String username = scanner.next();
+            String password = scanner.next();
+
+            String loginRequest = new LoginRequest(username, password).toString();
+            String json = gson.toJson(loginRequest);
+
+            String result = serverFacade.loginUser(json);
+            LoginResult loginResult = gson.fromJson(result, LoginResult.class);
+
+            registeredUsername = loginResult.username();
+            System.out.println("Welcome " + SET_TEXT_BOLD + registeredUsername + "!");
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            // TODO: request valid credentials
+        }
+    }
+
+    private static void DisplayCommands(String username) {
         if (username.isEmpty()) {
             System.out.println(SET_TEXT_COLOR_MAGENTA + "    register <USERNAME> <PASSWORD> <EMAIL>" + RESET_TEXT_COLOR + " - to create an account");
             System.out.println(SET_TEXT_COLOR_MAGENTA + "    login <USERNAME> <PASSWORD>" + RESET_TEXT_COLOR + " - to play chess");
