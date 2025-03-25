84fa21c717fa1a97f7cfa629f44d0ce7b6699f9e
Josh-Weidner <josh.weidner2@gmail.com>
2025-03-24 20:36:10 -0600

    implementing starting ui

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -2,25 +2,28 @@
 
 import static ui.EscapeSequences.*;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 //        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
 //        System.out.println("♕ 240 Chess Client: " + piece);
 
         System.out.println();
         System.out.println(BLACK_QUEEN + "Welcome to 240 Chess. Type 'ready' to get started." + BLACK_QUEEN);
-        // If not logged in
-            try {
-                String command = "export PS1='[LOGGED_OUT] >>> '";
-                ProcessBuilder pb = new ProcessBuilder("bash", "-c", command);
-                pb.inheritIO().start().waitFor();
-            } catch (IOException | InterruptedException e) {
-                e.printStackTrace();
-            }
-        System.out.println(SET_TEXT_COLOR_MAGENTA + "    register <USERNAME> <PASSWORD> <EMAIL>" + RESET_TEXT_COLOR + " - to create an account");
-        System.out.println(SET_TEXT_COLOR_MAGENTA + "    login <USERNAME> <PASSWORD>" + RESET_TEXT_COLOR + " - to play chess");
-        System.out.println(SET_TEXT_COLOR_MAGENTA + "    quit" + RESET_TEXT_COLOR + " - exit program");
-        System.out.println(SET_TEXT_COLOR_MAGENTA + "    help" + RESET_TEXT_COLOR + " - possible commands");
+
+        System.out.print("[LOGGED_OUT] >>> ");
+        String input = scanner.nextLine();
+
+        if (input.equalsIgnoreCase("ready")) {
+            System.out.println(SET_TEXT_COLOR_MAGENTA + "    register <USERNAME> <PASSWORD> <EMAIL>" + RESET_TEXT_COLOR + " - to create an account");
+            System.out.println(SET_TEXT_COLOR_MAGENTA + "    login <USERNAME> <PASSWORD>" + RESET_TEXT_COLOR + " - to play chess");
+            System.out.println(SET_TEXT_COLOR_MAGENTA + "    quit" + RESET_TEXT_COLOR + " - exit program");
+            System.out.println(SET_TEXT_COLOR_MAGENTA + "    help" + RESET_TEXT_COLOR + " - possible commands");
+        }
+
+        System.out.print("[LOGGED_OUT] >>> ");
+        String input2 = scanner.nextLine();
     }
 }
